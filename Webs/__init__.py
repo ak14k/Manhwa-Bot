92f6261f5d410f53e501aba1438987f00e7ea77b
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2025-07-06 19:59:20 +0545

    Update __init__.py

--- Webs/__init__.py
+++ Webs/__init__.py
@@ -1,10 +1,10 @@
 from .comick import ComickWebs
-from .mangamob import MangaMobWebs
+#from .mangamob import MangaMobWebs
 #from .mangafire import MangaFireWebs
 
 from .asurascans import AsuraScansWebs
-from .flamecomics import FlameComicsWebs
-from .demonicscans import DemonicScansWebs
+#from .flamecomics import FlameComicsWebs
+#from .demonicscans import DemonicScansWebs
 from .manhuafast import ManhuaFastWebs
 from .weebcentral import WeebCentralWebs
 from .manhwaclan import ManhwaClanWebs
