b6d6caf9145f3cdc2edd99427230633fd4045cd8
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2025-07-06 19:57:17 +0545

    Add files via upload

--- Webs/__init__.py
+++ Webs/__init__.py
@@ -1,1 +1,13 @@
+from .comick import ComickWebs
+from .mangamob import MangaMobWebs
+#from .mangafire import MangaFireWebs
 
+from .asurascans import AsuraScansWebs
+from .flamecomics import FlameComicsWebs
+from .demonicscans import DemonicScansWebs
+from .manhuafast import ManhuaFastWebs
+from .weebcentral import WeebCentralWebs
+from .manhwaclan import ManhwaClanWebs
+from .templetoons import TempleToonsWebs
+from .manhuaplus import ManhuaplusWebs
+from .mgeko import MgekoWebs
